70073bc3f53046902f23bdd620c11abfb517a09b
Valeriy Kliuk <valeriy.kliuk@gmail.com>
2023-11-15 10:49:43 -0500

    initial Xcode commit

---
+++ TikTokLikeApp/TikTokLikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokLikeAppApp.swift
+//  TikTokLikeApp
+//
+//  Created by Valery on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct TikTokLikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
